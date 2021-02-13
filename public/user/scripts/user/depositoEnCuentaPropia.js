4c3790b237e5d1ed2142217793dd08aeaf5827e6
cristian daniel herrera <cristian.daniel.herrera@hotmail.com>
2021-02-13 16:11:45 +0000

    homebanking

--- public/user/scripts/user/depositoEnCuentaPropia.js
+++ public/user/scripts/user/depositoEnCuentaPropia.js
@@ -23,14 +23,14 @@ function logoutOff() {
     // borrar usuario
     localStorage.removeItem("user");
     //ir a index
-    location.href = "../../public/user/index.html";
+    location.href = "../../index.html";
   }
 }
 
 let volver = document.getElementById("volver");
 volver.addEventListener("click", volverIndex2);
 function volverIndex2() {
-  location.href = "../../public/user/index2.html";
+  location.href = "../../index2.html";
 }
 
 // let bienvenido = document.getElementById('seccion');
